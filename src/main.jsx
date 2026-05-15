574378780a1134104065e55b69e0fc5fbd9fdc46
kalleindradeep <126305213+kalleindradeep@users.noreply.github.com>
2026-05-15 18:08:49 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PortfolioSite from "./App";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PortfolioSite />
+  </React.StrictMode>
+);
